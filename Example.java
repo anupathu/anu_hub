f0d01b2e0ad280eb446dc3cd58438a68b09dac76
anupathu <anuyadala863@gmail.com>
2023-02-25 23:48:53 +0530

    second commit

---
+++ Example.java
@@ -0,0 +1,12 @@
+//Without return type and without parametrs;
+class Example{
+ void method(){
+ int a = 22;
+int b = 33;
+System.out.println(a); 
+}
+public static void main(String [] args){
+Example m1 =new Example();
+m1.method();
+}
+}
