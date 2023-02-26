e2d889bdf2ad46a62c1138be8e9000c08cde23f1
anupathu <anuyadala863@gmail.com>
2023-02-26 21:17:41 +0530

    Good grace

---
+++ Datatypes.java
@@ -0,0 +1,29 @@
+class Marks{
+public static void main(String []args){
+int standard = 10;
+int rollNo = 2;
+double mark = 50.50;
+int rank = 42;
+char grade = 'D';
+boolean status = true;
+int phone = 344544;
+
+
+
+
+System.out.println("Z.P.H School Results Student Name : Anusha");
+
+
+System.out.println(standard);
+System.out.println(rollNo);
+
+System.out.println(mark);
+
+System.out.println(grade);
+
+System.out.println(status);
+System.out.println(phone);
+
+
+}
+}
